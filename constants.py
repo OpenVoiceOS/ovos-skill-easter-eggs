33f1aa46c970f3b4af408e5e6efcdf7e35597613
Mike Gray <mike@graywind.org>
2023-05-09 22:53:23 -0500

    feat: grandma mode

---
+++ constants.py
@@ -0,0 +1,15 @@
+SPICY_SOUNDS = [
+    "sounds/arnold/bullsh_dont.wav",
+    "sounds/arnold/fist.wav",
+    "sounds/arnold/no_sh.wav",
+    "sounds/arnold/sob.wav",
+    "sounds/arnold/t1_fu.wav",
+    "sounds/arnold/ugly.wav",
+    "sounds/bender/Bodies are for hookers.mp3",
+    "sounds/bender/Compare your lives to mine.mp3",
+    "sounds/bender/Dont make me kick your neck.mp3",
+    "sounds/bender/Im gonna go kill my self.mp3",
+    "sounds/bender/Lets go get drunk.mp3",
+    "sounds/bender/That dirty bastard.mp3",
+    "sounds/bender/Wanna kill all humans.mp3",
+]
